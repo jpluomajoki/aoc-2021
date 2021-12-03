bafc7a47ad015e83442eaa6bb0e58502fcd76f4d
jpluomajoki <jpluomajoki@gmail.com>
2021-12-03 09:37:18 +0200

    Solve day 3 part 1

---
+++ src/aoc_2021/day_3.clj
@@ -0,0 +1,4 @@
+(ns aoc-2021.day-3
+  (:require [aoc-2021.utils :refer [read-test-input read-input]])
+
+(def testinput (read-test-ipnut "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n"))
